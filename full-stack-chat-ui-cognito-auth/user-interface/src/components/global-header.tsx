e4d95da66f4434facdb8d7c042c31a2507d9e307
Anthony <brnaba@amazon.fr>
2024-10-04 15:47:30 +0200

    added logout

--- full-stack-chat-ui-cognito-auth/user-interface/src/components/global-header.tsx
+++ full-stack-chat-ui-cognito-auth/user-interface/src/components/global-header.tsx
@@ -1,17 +1,47 @@
-import { useState } from "react";
-import { TopNavigation } from "@cloudscape-design/components";
+import { useEffect, useState } from "react";
+import { signOut, fetchAuthSession } from "aws-amplify/auth";
+import {
+  ButtonDropdownProps,
+  TopNavigation,
+} from "@cloudscape-design/components";
 import { Mode } from "@cloudscape-design/global-styles";
 import { StorageHelper } from "../common/helpers/storage-helper";
 import { APP_NAME } from "../common/constants";
+import { useOnFollow } from "../common/hooks/use-on-follow";
 
 export default function GlobalHeader() {
+  const onFollow = useOnFollow();
+  const [userName, setUserName] = useState<string | null>(null);
   const [theme, setTheme] = useState<Mode>(StorageHelper.getTheme());
+
+  useEffect(() => {
+    (async () => {
+      const session = await fetchAuthSession();
+
+      if (!session) {
+        signOut();
+        return;
+      }
+
+      setUserName(session.tokens?.idToken?.payload?.email?.toString() ?? "");
+    })();
+  }, []);
 
   const onChangeThemeClick = () => {
     if (theme === Mode.Dark) {
       setTheme(StorageHelper.applyTheme(Mode.Light));
     } else {
       setTheme(StorageHelper.applyTheme(Mode.Dark));
+    }
+  };
+
+  const onUserProfileClick = ({
+    detail,
+  }: {
+    detail: ButtonDropdownProps.ItemClickDetails;
+  }) => {
+    if (detail.id === "signout") {
+      signOut();
     }
   };
 
@@ -30,6 +60,19 @@ export default function GlobalHeader() {
             type: "button",
             text: theme === Mode.Dark ? "Light Mode" : "Dark Mode",
             onClick: onChangeThemeClick,
+          },
+          {
+            type: "menu-dropdown",
+            description: userName ?? "",
+            iconName: "user-profile",
+            onItemClick: onUserProfileClick,
+            items: [
+              {
+                id: "signout",
+                text: "Sign out",
+              },
+            ],
+            onItemFollow: onFollow,
           },
         ]}
       />
